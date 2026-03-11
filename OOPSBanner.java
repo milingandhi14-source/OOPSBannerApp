2f3d1762126d52b0a0f74b0dce7e05e2ca21faf7
mg1249 <mg1249@srmist.edu.in>
2026-03-11 22:18:13 +0530

    Milin Added: UC1 Print OOPS banner

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -3,7 +3,11 @@ public class OOPSBanner {
     public static void main(String[] args) {
 
         System.out.println("Welcome to OOPSBanner application");
+        System.out.println("OOPS Concepts:");
+        System.out.println("1. Encapsulation");
+        System.out.println("2. Inheritance");
+        System.out.println("3. Polymorphism");
+        System.out.println("4. Abstraction");
 
     }
-
 }
