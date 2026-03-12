e718e272c496111a6b981c532b3ad293e66680a4
mg1249 <mg1249@srmist.edu.in>
2026-03-12 10:12:29 +0530

    UC7 implemented using CharacterPattern class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,58 +1,81 @@
+/*
+ * OOPSBannerApp – Combined UC1 to UC7
+ * UC7 introduces CharacterPattern class to store banner patterns
+ */
+
 public class OOPSBannerApp {
+
+    // UC7: Class to store character and its pattern
+    static class CharacterPattern {
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
 
     public static void main(String[] args) {
 
+        // UC1: Intro text
         System.out.println("Welcome to OOPSBanner application");
         System.out.println("OOPS Concepts:");
         System.out.println("1. Encapsulation");
         System.out.println("2. Inheritance");
         System.out.println("3. Polymorphism");
         System.out.println("4. Abstraction");
-
         System.out.println();
 
-        String[] O = buildO();
-        String[] P = buildP();
-        String[] S = buildS();
+        // UC7: Create character pattern objects
+        CharacterPattern O = new CharacterPattern('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
 
+        CharacterPattern P = new CharacterPattern('P', new String[]{
+                "**** ",
+                "*   *",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[]{
+                " ****",
+                "*    ",
+                "*    ",
+                " *** ",
+                "    *",
+                "    *",
+                "**** "
+        });
+
+        // Store characters for word "OOPS"
+        CharacterPattern[] word = {O, O, P, S};
+
+        // Print banner
         for (int i = 0; i < 7; i++) {
-            System.out.println(String.join(" ", O[i], O[i], P[i], S[i]));
+            StringBuilder line = new StringBuilder();
+            for (CharacterPattern cp : word) {
+                line.append(cp.getPattern()[i]).append(" ");
+            }
+            System.out.println(line);
         }
-    }
-
-    public static String[] buildO() {
-        return new String[]{
-            " *** ",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            "*   *",
-            " *** "
-        };
-    }
-
-    public static String[] buildP() {
-        return new String[]{
-            "**** ",
-            "*   *",
-            "*   *",
-            "**** ",
-            "*    ",
-            "*    ",
-            "*    "
-        };
-    }
-
-    public static String[] buildS() {
-        return new String[]{
-            " ****",
-            "*    ",
-            "*    ",
-            " *** ",
-            "    *",
-            "    *",
-            "**** "
-        };
     }
 }
