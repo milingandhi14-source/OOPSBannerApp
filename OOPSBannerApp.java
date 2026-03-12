aeaf41d0bdaa8872da18d15fa890f8a6c2312543
mg1249 <mg1249@srmist.edu.in>
2026-03-12 10:05:33 +0530

    UC6 implemented using helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,40 +1,27 @@
-/*
- * OOPSBannerApp - UC6 Final Version
- * Demonstrates UC1 to UC6 concepts
- * UC1: Print OOPS concepts
- * UC3: String.join()
- * UC4: Array + Loop
- * UC5: Array Initialization
- * UC6: Static helper methods for characters
- */
-
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // UC1 Content
         System.out.println("Welcome to OOPSBanner application");
         System.out.println("OOPS Concepts:");
         System.out.println("1. Encapsulation");
         System.out.println("2. Inheritance");
         System.out.println("3. Polymorphism");
         System.out.println("4. Abstraction");
+
         System.out.println();
 
-        // Get character patterns
         String[] O = buildO();
         String[] P = buildP();
         String[] S = buildS();
 
-        // Print banner using loop
         for (int i = 0; i < 7; i++) {
             System.out.println(String.join(" ", O[i], O[i], P[i], S[i]));
         }
     }
 
-    // Helper method for letter O
     public static String[] buildO() {
-        return new String[] {
+        return new String[]{
             " *** ",
             "*   *",
             "*   *",
@@ -45,9 +32,8 @@ public class OOPSBannerApp {
         };
     }
 
-    // Helper method for letter P
     public static String[] buildP() {
-        return new String[] {
+        return new String[]{
             "**** ",
             "*   *",
             "*   *",
@@ -58,9 +44,8 @@ public class OOPSBannerApp {
         };
     }
 
-    // Helper method for letter S
     public static String[] buildS() {
-        return new String[] {
+        return new String[]{
             " ****",
             "*    ",
             "*    ",
